560363ae069b90707bcb8f99dcd10ee58430d92e
Matěj Melichna <melimahu@gmail.com>
2018-04-22 11:05:06 -0700

    Added Populations.py file

---
+++ Populations.py
@@ -0,0 +1,40 @@
+import random, matplotlib.pyplot as plt
+
+amount = int()
+startingAmount = input ("How many members will be at the start?: ")
+startingAmount = int (startingAmount)
+amount = startingAmount
+time = int()
+timeArray = int()
+timeArray = [0]
+amountArray = [startingAmount]
+adding = int()
+adding = [1,-1]
+choice = int()
+
+def plotResults (plotTime, plotAmount):
+	#plt.axis([0, time, startingAmount, amount])
+	plt.title("Population oscilation")
+	plt.xlabel("Time")
+	plt.ylabel("Amount of members of population")
+	plt.plot(plotTime, plotAmount)
+	#plt.table(cellText=None, cellColours=None, cellLoc='right', colWidths=None, rowLabels=None, rowColours=None, rowLoc='left', colLabels=None, colColours=None , colLoc='center',loc='bottom', bbox=None)
+	plt.show()
+	
+
+while (amount > 0):
+	time = time + 1
+	timeArray.append(time)
+	choice = random.choice(adding)
+	amount = amount + choice
+	amountArray.append(amount)
+	print (amount)
+
+print ("Your population gone extinct after ",time,"periods.")
+plotResults(timeArray, amountArray)
+
+
+
+	
+
+
