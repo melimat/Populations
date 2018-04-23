dd9537fc006851c4e20e70c70388fb50e0f14fc5
Matěj Melichna <melimahu@gmail.com>
2018-04-23 12:25:34 -0700

    Functional rework

--- Populations.py
+++ Populations.py
@@ -1,13 +1,7 @@
 import random, matplotlib.pyplot as plt
 
-startingAmount = input ("How many members will be at the start?: ")
-startingAmount = int (startingAmount)
-amount = int(startingAmount)
-timeArray = [0]
-amountArray = [startingAmount]
-adding = [1,-1]
-choice = int()
-time = int()
+startingAmount = int (input ("How many members will be at the start?: "))
+
 
 def plotResults (plotTime, plotAmount, timeAmount):
 	#plt.axis([0, time, startingAmount, amount])
@@ -19,20 +13,24 @@ def plotResults (plotTime, plotAmount, timeAmount):
 	#plt.table(cellText=None, cellColours=None, cellLoc='right', colWidths=None, rowLabels=None, rowColours=None, rowLoc='left', colLabels=None, colColours=None , colLoc='center',loc='bottom', bbox=None)
 	plt.show()
 	
+def extinctionSym (startingAmount):
+    amount = int(startingAmount)
+    time = int()
+    timeArray = [0]
+    amountArray = [startingAmount]
+    adding = [1,-1]
+    choice = int()
+    while (amount > 0):
+    	time = time + 1
+    	timeArray.append(time)
+    	choice = random.choice(adding)
+    	amount = amount + choice
+    	amountArray.append(amount)
+    	print (amount)
+    return (timeArray, amountArray, time)
 
-while (amount > 0):
-	time = time + 1
-	timeArray.append(time)
-	choice = random.choice(adding)
-	amount = amount + choice
-	amountArray.append(amount)
-	print (amount)
-
-
-plotResults(timeArray, amountArray, time)
-
-
-
+generated_data = extinctionSym(startingAmount)
+plotResults (generated_data[0], generated_data[1], generated_data[2])
 	
 
 
