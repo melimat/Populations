d3572a040a11e96c21267e2a9dc3393d139cc7ce
Matěj Melichna <melimahu@gmail.com>
2018-07-17 19:27:20 +0200

    Code refactoring

--- Populations-environment.py
+++ Populations-environment.py
@@ -1,22 +1,16 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt, random
 
-
-environmentCapacity = int(input("Maximal capacity that nevironment can handle?: "))
+environmentCapacity = int(input("Maximal capacity that environment can handle?: "))
 
 global startingAmount
-startingAmount = int(input("How many memmbers wolud be at the beggining?: "))
+startingAmount = int(input("How many members would be at the beginning?: "))
 
 global maxTime
 maxTime = int(input("How long may simulation take?: "))
 
 global a
 a = int(input("Max Increasing/Decreasing value?: "))
-
-#environmentCapacity = 15
-#startingAmount = 10
-#maxTime = 100
-#a = 2
 
 global amountArray
 amountArray = [startingAmount]
@@ -47,7 +41,7 @@ def oscillationSym(capacity, start, maxtime):
             amountArray.append(amount)
         if (amount > maxAmount):
             maxAmount = amount
-        print((time, " : ", amount, " -> ", maxAmount))
+        print(("Time: " + str(time) + " : " + "Amount: " + str(amount) + " -> " + "maxAmount: " +  str(maxAmount)))
         if (amount <= 0):
             amountArray.append(0)
             extinct = True
@@ -57,9 +51,9 @@ def oscillationSym(capacity, start, maxtime):
 def plotResults(plotTime, plotAmount, envCap):
     global maxAmount, startingAmount, extinct
     if (extinct is True):
-        print ("Cannot plot garph, your population has gone extinct.")
+        print("Cannot plot graph, your population has gone extinct.")
     else:
-        plt.title("Population oscillating arround environmental capacity")
+        plt.title("Population oscillating around environmental capacity")
         plt.xlabel("Time")
         plt.ylabel("Amount of members of population")
         plt.xlim(0, maxTime)
@@ -68,7 +62,7 @@ def plotResults(plotTime, plotAmount, envCap):
         for i in range(len(plotTime)):
             capArray.append(envCap)
         plt.plot(plotTime, capArray, label="Environmental capacity")
-        plt.plot(plotTime, plotAmount, label="Poulation amount")
+        plt.plot(plotTime, plotAmount, label="Population amount")
         plt.legend()
         plt.show()
 
--- Populations.py
+++ Populations.py
@@ -1,7 +1,7 @@
 import random, matplotlib.pyplot as plt
 
 a = int(input("Increasing/decreasing value?: "))
-startingAmount = int (input ("How many members will be at the start?: "))
+startingAmount = int(input("How many members will be at the start?: "))
 global adding
 adding = [a, -a]
 global timeArray
@@ -11,45 +11,32 @@ amountArray = [startingAmount]
 global time
 
 
-
-
-def plotResults (plotTime, plotAmount):
-    timeLenght = int(len(plotTime)-1)
-    #plt.axis([0, time, startingAmount, amount])
-    print ("Your population has gone extinct after ",timeLenght,"periods.")
+def plotResults(plotTime, plotAmount):
+    timeLenght = int(len(plotTime) - 1)
+    print("Your population has gone extinct after " + str(timeLenght) + " periods.")
     plt.title("Population oscilation")
     plt.xlabel("Time")
     plt.ylabel("Amount of members of population")
     plt.plot(plotTime, plotAmount)
-    #plt.table(cellText=None, cellColours=None, cellLoc='right', colWidths=None, rowLabels=None, rowColours=None, rowLoc='left', colLabels=None, colColours=None , colLoc='center',loc='bottom', bbox=None)
     plt.show()
-	
-def extinctionSym (startingAmount):
+
+
+def extinctionSym(startingAmount):
     amount = int(startingAmount)
     time = int()
-    #timeArray = [0]
-    #amountArray = [startingAmount]
-    #adding = [1,-1]
-    choice = int()
     while (amount > 0):
-    	time = time + 1
-    	timeArray.append(time)
-    	choice = random.choice(adding)
-    	amount = amount + choice
-    	if (amount >= 0):
-    	    amountArray.append(amount)
-    	    print (amount)
-    	else:
-    	    amount = 0
-    	    amountArray.append(amount)
-    	    print (amount)
-            	
-    #return (timeArray, amountArray, time)
+        time = time + 1
+        timeArray.append(time)
+        choice = random.choice(adding)
+        amount = amount + choice
+        if (amount >= 0):
+            amountArray.append(amount)
+            print(amount)
+        else:
+            amount = 0
+            amountArray.append(amount)
+            print(amount)
 
-#generated_data = extinctionSym(startingAmount)
-#plotResults (generated_data[0], generated_data[1], generated_data[2])
+
 extinctionSym(startingAmount)
 plotResults(timeArray, amountArray)
-	
-
-
